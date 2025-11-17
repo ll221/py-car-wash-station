2203ef68fbadc2751fcaa520467cffb88fa9d646
Ihor843 <ihor.kunakh.kb.2022@lpnu.ua>
2025-11-17 16:41:07 +0200

    finalll

--- app/main.py
+++ app/main.py
@@ -2,7 +2,12 @@ from typing import List
 
 
 class Car:
-    def __init__(self, comfort_class: int, clean_mark: int, brand: str) -> None:
+    def __init__(
+        self,
+        comfort_class: int,
+        clean_mark: int,
+        brand: str
+    ) -> None:
         self.comfort_class: int = comfort_class
         self.clean_mark: int = clean_mark
         self.brand: str = brand
